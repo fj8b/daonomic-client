7469431bc82d16e03a0d4085c52fb173c711768a
Dev <dev@0v1se.com>
2018-03-06 23:04:03 +0300

    sale timeline fix

--- source/config/sale-timeline.js
+++ source/config/sale-timeline.js
@@ -11,28 +11,10 @@ export default [
     title: 'Sale Timeline:',
     steps: [
       {
-        date: '21 Jul',
-        text: '1 Token Coin = 30 BTC',
+        date: '25 Feb',
+        text: '1 GWON = $1',
         percent: '30%',
-        isActive: false,
-      },
-      {
-        date: '21 Aug',
-        text: '1 Token Coin = 40 BTC',
-        percent: '20%',
-        isActive: false,
-      },
-      {
-        date: '21 Sep',
-        text: '1 Token Coin = 50 BTC',
-        percent: '10%',
         isActive: true,
-      },
-      {
-        date: '21 Oct',
-        text: '1 Token Coin = 40 BTC',
-        percent: '5%',
-        isActive: false,
       },
     ],
   },
